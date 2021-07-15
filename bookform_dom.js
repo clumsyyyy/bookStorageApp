b8f33ed70eaf5dbfa22bc82b85fcb953422fa48f
clumsyyyy <71161031+clumsyyyy@users.noreply.github.com>
2021-07-15 15:47:09 +0700

    Add files via upload

--- bookform_dom.js
+++ bookform_dom.js
@@ -23,7 +23,10 @@ function addBook(){
     bookList.append(book);
     console.log(title, author, year, completion);
 
+
     updateData();
+
+    
 }
 
 function createBook(title, author, year, completion){
@@ -31,7 +34,8 @@ function createBook(title, author, year, completion){
     const bookTitle = document.createElement("h3");
     bookTitle.innerText = title;
     bookTitle.style["margin-right"] = "2%";
-    bookTitle.style["display"] = "inline-block";
+    bookTitle.style["margin-bottom"] = "-20px";
+    bookTitle.style["margin-top"] = "20px";
 
     const bookAuthor = document.createElement("h4");
     bookAuthor.innerText = author;
@@ -49,30 +53,36 @@ function createBook(title, author, year, completion){
 
     const container = document.createElement("div");
     container.classList.add("item", "shadow");
+    container.style["padding"] = "5px";
     container.append(bookContainer);
 
     const deleteButton = createDeleteButton();
     deleteButton.innerText = "Delete"
+    deleteButton.style["margin-top"] = "-70px";
+    deleteButton.style["display"] = "inline-block";
     container.append(deleteButton)
     
 
     if(completion === true){
         const restoreButton = createRestoreButton();
         restoreButton.innerText = "Restore"
+        restoreButton.style["margin-top"] = "-70px";
+        restoreButton.style["margin-right"] = "110px";
         container.append(restoreButton)
 
     } else {
         const finishedButton = createFinishedButton();
         finishedButton.innerText = "Finish"
+        finishedButton.style["margin-top"] = "-70px";
+        finishedButton.style["margin-right"] = "110px";
         container.append(finishedButton)
     }    
-
+    container.setAttribute("class", "listBox")
     container.style["backgroundColor"] = "rgb(204, 204, 204)";
     container.style["borderRadius"] = "20px";
     container.style["paddingLeft"] = "20px";
-    container.style["height"] = "150px";
+    container.style["height"] = "80px";
     container.style["margin-bottom"] = "20px";
-    
     return container;
 }
 
@@ -97,6 +107,7 @@ function createSearchBook(title, author, year, completion){
     bookContainer.append(bookTitle, bookAuthor, bookYear);
 
     const container = document.createElement("div");
+    container.setAttribute("class", "searchedBox")
     container.classList.add("item", "shadow");
     container.style["backgroundColor"] = "rgb(204, 204, 204)";
     container.style["borderRadius"] = "20px";
@@ -116,7 +127,7 @@ function createButton(buttonTypeClass , eventListener) {
     return button;
 }
 
-function addBookToCompleted(taskElement){
+function finishBook(taskElement){
     const bookTitle = taskElement.querySelector(".inner > h3").innerText;
     const bookAuthor = taskElement.querySelector(".inner > h4").innerText;
     const bookYear = taskElement.querySelector(".inner > p").innerText;
@@ -131,9 +142,11 @@ function addBookToCompleted(taskElement){
     taskElement.remove();
 
     updateData();
+    const string = "The book " + bookTitle + " has been succesfully moved to the Finished list."
+    setTimeout(alert.bind(null, string));
 }
 
-function UndoBookFromCompleted(taskElement){
+function restoreBook(taskElement){
     const bookTitle = taskElement.querySelector(".inner > h3").innerText;
     const bookAuthor = taskElement.querySelector(".inner > h4").innerText;
     const bookYear = taskElement.querySelector(".inner > p").innerText;
@@ -147,19 +160,24 @@ function UndoBookFromCompleted(taskElement){
 
     uncompletedBooks.append(newBook);
     taskElement.remove();
-
     updateData();
+    const string = "The book " + bookTitle + " has been succesfully moved to the Unfinished list."
+    setTimeout(alert.bind(null, string));
 }
 
 function removeBook(taskElement){
+    const bookTitle = taskElement.querySelector(".inner > h3").innerText;
     const bookPosition = findBookIndex(taskElement[bookItemID]);
     books.splice(bookPosition, 1);
     taskElement.remove();
     updateData();
+    const string = "The book " + bookTitle + " has been succesfully deleted."
+    setTimeout(alert.bind(null, string));
 }
 function createFinishedButton(){
     return createButton("finished-button", function(event){
-        addBookToCompleted(event.target.parentElement);
+        finishBook(event.target.parentElement);
+        
     })
 }
 
@@ -171,6 +189,6 @@ function createDeleteButton(){
 
 function createRestoreButton(){
     return createButton("restore-button", function(event){
-        UndoBookFromCompleted(event.target.parentElement);
+        restoreBook(event.target.parentElement);
     });
 }
--- bookform_listener.js
+++ bookform_listener.js
@@ -3,6 +3,15 @@ document.addEventListener("DOMContentLoaded", function(){
     submitForm.addEventListener("submit", function(event){
         event.preventDefault();
         addBook();
+        const completion = document.getElementById("isCompleted").checked;
+        const submitTitle = document.getElementById("title").value;
+        if (completion){
+            const string = "The book " + submitTitle + " has successfully been added into the Finished book list."
+            setTimeout(alert.bind(null, string));
+        } else {
+            const string = "The book " + submitTitle + " has successfully been added into the Unfinished book list."
+            setTimeout(alert.bind(null, string));
+        }
     });
     
     const searchForm = document.getElementById("searchForm");
@@ -12,6 +21,7 @@ document.addEventListener("DOMContentLoaded", function(){
         const splittedSearchArr = searchArr.split("=");
         const identifier = splittedSearchArr[0];
         const keyword = splittedSearchArr[1];
+        console.log(splittedSearchArr)
         console.log(keyword)
         const searchBookBox = document.getElementById("searchedBookListDetail");
         searchBookBox.innerHTML = "";
@@ -19,50 +29,59 @@ document.addEventListener("DOMContentLoaded", function(){
         const bookKeyArr = JSON.parse(bookKey)
         console.log(bookKeyArr)
         if (identifier === "title"){
-            
             bookKeyArr.forEach((element) => {
-                console.log(element.title)
-                var isFound = false;
                 const title = element.title;
-                if (title.length >= keyword.length){
+                if (title.length > keyword.length){
                     titleSplitArr = title.split(" ");
-                    console.log(titleSplitArr)
                     for(let i = 0; i < titleSplitArr.length; i++){
-                        console.log(titleSplitArr[i])
+                        var isFound = false;
+                       
                         if (titleSplitArr[i].toLowerCase() === keyword.toLowerCase()){
                             isFound = true;
-                        }
+                        };
+
+                        if (isFound === true){
+                            const bookBox = createSearchBook(element.title, element.author, element.year, element.isCompleted);
+                            searchBookBox.append(bookBox);
+                        };
+                    };
+                    
+                }else if(title.length === keyword.length){
+                    if (title.toLowerCase() === keyword.toLowerCase()){
+                        const bookBox = createSearchBook(element.title, element.author, element.year, element.isCompleted);
+                        searchBookBox.append(bookBox);
                     }
                 }
-                if (isFound == true){
-                    const bookBox = createSearchBook(element.title, element.author, element.year, element.isCompleted);
-                    searchBookBox.append(bookBox);
-                }
-
             });
         } else if (identifier === "author"){
+            
             bookKeyArr.forEach((element) => {
-                var isFound = false;
                 const author = element.author;
-                if (author.length >= keyword.length){
+                console.log(author.toLowerCase(), author.length, keyword.toLowerCase(), keyword.length)
+                if (author.length > keyword.length){
                     authorSplitArr = author.split(" ");
-                    console.log(authorSplitArr)
                     for(let i = 0; i < authorSplitArr.length; i++){
-                        console.log(authorSplitArr[i])
+                        var isFound = false;
+                       
                         if (authorSplitArr[i].toLowerCase() === keyword.toLowerCase()){
                             isFound = true;
-                        }
+                        };
+
+                        if (isFound === true){
+                            const bookBox = createSearchBook(element.title, element.author, element.year, element.isCompleted);
+                            searchBookBox.append(bookBox);
+                        };
+                    };
+                    
+                }else if(author.length === keyword.length){
+                    if (author.toLowerCase() === keyword.toLowerCase()){
+                        const bookBox = createSearchBook(element.title, element.author, element.year, element.isCompleted);
+                        searchBookBox.append(bookBox);
                     }
                 }
-                if (isFound == true){
-                    const bookBox = createSearchBook(element.title, element.author, element.year, element.isCompleted);
-                    searchBookBox.append(bookBox);
-                }
-
             });
         } else if (identifier === "year"){
             bookKeyArr.forEach((element) => {
-                var isFound = false;
                 const year = element.year;
                 if (year == keyword){
                     const bookBox = createSearchBook(element.title, element.author, element.year, element.isCompleted);
